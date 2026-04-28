b8af1792f3f661c747f065d5251e764244dc3649
ZaZaLee <zaza.viking@gmail.com>
2026-04-28 11:47:02 +0800

    修改老版本git操作

--- scripts/build_harbor_hermes_image.sh
+++ scripts/build_harbor_hermes_image.sh
@@ -6,8 +6,11 @@ ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 SOURCE_DOCKERFILE="${ROOT_DIR}/Dockerfile.ghcr"
 
 echo "Updating source tree in ${ROOT_DIR}"
-git -C "${ROOT_DIR}" fetch origin
-git -C "${ROOT_DIR}" reset --hard origin/main
+(
+  cd "${ROOT_DIR}"
+  git fetch origin
+  git reset --hard origin/main
+)
 
 HARBOR_REGISTRY="${HARBOR_REGISTRY:-127.0.0.1:81}"
 HARBOR_URL="${HARBOR_URL:-http://127.0.0.1:81}"
